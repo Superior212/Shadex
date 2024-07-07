b26a6473e45b1961cfbaff90702c8b1b301c61a5
Samson Aderonmu <143148845+samsonHRC@users.noreply.github.com>
2024-07-07 14:48:44 +0100

    refactor: Improve accessibility and consistency of Cart component

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -153,14 +153,18 @@ export default function Cart() {
                           className="aspect-square rounded-md object-cover"
                         />
                         <div>
-                          <p className="font-bold">{item.name}</p>
-                          <span>
+                          <p className="font-[700] text-xl garamond my-2">
+                            {item.name}
+                          </p>
+                          <span className="lato text-base font-[400]">
                             Color: {item.color}, Material: {item.material}
                           </span>
                         </div>
                       </div>
                       <div className="mt-2">
-                        <p className="font-bold">₦ {item.price.toFixed(2)}</p>
+                        <p className="font-[400] text-base my-2 lato">
+                          ₦ {item.price.toFixed(2)}
+                        </p>
                       </div>
                       <div className="flex items-center gap-2 mt-2">
                         <Button
@@ -184,7 +188,7 @@ export default function Cart() {
                         </Button>
                       </div>
                       <div className="mt-2">
-                        <p>
+                        <p className="my-2 font-[400] text-base  lato">
                           Total: ₦{" "}
                           {(item.price * cartItems[item.id]).toFixed(2)}
                         </p>
@@ -198,9 +202,9 @@ export default function Cart() {
           </div>
           {totalPrice > 0 ? (
             <Card className="bg-[#EEFFFC]">
-              <CardContent className="flex items-center justify-between px-5 py-2 max-w-[80rem] mx-auto">
+              <CardContent className="flex sm:flex-row flex-col sm:items-center justify-center sm:justify-between sm:px-5 px-8 py-2 max-w-[80rem] sm:mx-auto">
                 <div className="">
-                  <div className="grid gap-2">
+                  <div className="grid  gap-2 py-5 sm:py-0">
                     <Label className="garamond text-xl" htmlFor="shipping-mode">
                       Choose Shipping Mode
                     </Label>
@@ -208,16 +212,16 @@ export default function Cart() {
                       id="shipping-mode"
                       value={shippingMode}
                       onValueChange={setShippingMode}
-                      className="gap-4">
+                      className="gap-4 ">
                       <Label
                         htmlFor="standard"
-                        className="flex items-center gap-2 cursor-pointer">
+                        className="flex items-center gap-2 my-3  sm:my-0  cursor-pointer">
                         <RadioGroupItem id="standard" value="standard" />
                         Door Step Delivery
                       </Label>
                       <Label
                         htmlFor="express"
-                        className="flex items-center gap-2 cursor-pointer">
+                        className="flex items-center  my-3  sm:my-0   gap-2 cursor-pointer">
                         <RadioGroupItem id="express" value="express" />
                         Store Pick-up - Free
                       </Label>
@@ -225,11 +229,11 @@ export default function Cart() {
                   </div>
                 </div>
                 <div className="grid gap-4">
-                  <div className="flex items-center gap-2 justify-between">
+                  <div className="flex items-center gap-2 my-3  sm:my-0  justify-between">
                     <p className="font-medium">Subtotal</p>{" "}
                     <span>₦{totalPrice}</span>
                   </div>
-                  <div className="flex items-center justify-between">
+                  <div className="flex items-center my-3  sm:my-0  justify-between">
                     <span className="font-medium">Shipping</span>
                     <span className="text-sm mx-4">
                       {shippingMode === "standard"
@@ -238,13 +242,13 @@ export default function Cart() {
                     </span>
                   </div>
                   <Separator />
-                  <div className="flex items-center justify-between">
+                  <div className="flex items-center my-3 sm:my-0 justify-between">
                     <span className="font-medium">Total</span>
                     <span>₦{totalPrice}</span>
                   </div>
                 </div>
               </CardContent>
-              <CardFooter className="flex justify-end gap-2">
+              <CardFooter className="flex sm:justify-end gap-2 my-3 sm:my-0">
                 <Button
                   onClick={handleProceedToCheckout}
                   className="bg-transparent hover:bg-transparent text-[#3A3A3A] border border-[#3A3A3A]">
